63a959bdbd87f9e70172c8756d58c3c31eba1079
Zacks Tsang <zshaojia@gmail.com>
2013-11-11 11:38:54 +0800

    add controller

---
+++ TestAutoLayout/TestAutoLayout/UIViewController+AutoController.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AutoController.h
+//  TestAutoLayout
+//
+//  Created by Zacks Tsang  on 13-11-11.
+//  Copyright (c) 2013年 www.aiysea.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (AutoController)
+
+@end
---
+++ TestAutoLayout/TestAutoLayout/UIViewController+AutoController.m
@@ -0,0 +1,13 @@
+//
+//  UIViewController+AutoController.m
+//  TestAutoLayout
+//
+//  Created by Zacks Tsang  on 13-11-11.
+//  Copyright (c) 2013年 www.aiysea.com. All rights reserved.
+//
+
+#import "UIViewController+AutoController.h"
+
+@implementation UIViewController (AutoController)
+
+@end
